9c723279b38ab5b9128229af0a1684153a2fd61e
anoopeashok <anoop.e.ashok@gmail.com>
2024-11-26 09:24:51 +0000

    nextconfig changed to default

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",  // <=== enables static exports
-  basePath: "/my-portfolio",
-  reactStrictMode: true,
-   images: { unoptimized: true }
 };
 
 export default nextConfig;
